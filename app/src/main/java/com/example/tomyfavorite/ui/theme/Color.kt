1b2c8350e25979c1912dd6630a7ddae08511913a
2xHjin <sbs98010@naver.com>
2026-02-15 22:33:46 +0900

    init: 프로젝트 초기 기본 폴더구조 생성

---
+++ app/src/main/java/com/example/tomyfavorite/ui/theme/Color.kt
@@ -0,0 +1,16 @@
+package com.example.tomyfavorite.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val PinkPastel = Color(0xFFFFE4E1) // 메인 핑크
+val PurplePastel = Color(0xFFF3E5F5) // 포인트 보라
+val DiaryWhite = Color(0xFFFFF9F9) // 다이어리 속지 느낌의 배경색
+val TextGray = Color(0xFF4A4A4A) // 부드러운 글자 색상
